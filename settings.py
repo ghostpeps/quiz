1d06a17735fa06dc27053cfc6a08328992952b10
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-04-05 20:09:50 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+pic = None
 def login_screen():
     st.title("Please Login.")
     st.button("Log in with Google", on_click=st.login, icon=":material/login:")
@@ -7,5 +8,10 @@ if not st.experimental_user.is_logged_in:
     login_screen()
 else:
     st.title("Settings")
+    if pic is None:
+        pic = ""
+    st.write("Profile Picture:")
+    st.image(image=pic, width=218)
+    st.button(label="Change Profile Picture")
     st.write(f"Username: {st.experimental_user}")
     st.button("Log out", on_click=st.logout, icon=":material/logout:")
