c3f33d12666a0051a330feb423182b93469a1c4f
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-04-05 19:44:23 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 def login_screen():
-    st.header("This app is private.")
-    st.subheader("Please log in.")
-    st.button("Log in with Google", on_click=st.login)
+    st.title("Please Login.")
+    st.button("Log in with Google", on_click=st.login, icon=":material/login:")
 if not st.experimental_user.is_logged_in:
     login_screen()
 else:
-    st.experimental_user
-    st.button("Log out", on_click=st.logout)
+    st.title("Settings")
+    st.write(f"Username: {st.experimental_user}")
+    st.button("Log out", on_click=st.logout, icon=":material/logout:")
