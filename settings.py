659d930d31778550fb083f8083ce767b8b0cf654
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-04-06 11:27:56 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ def login_screen():
               <span class="gsi-material-button-contents" style="font-size: 16px;">Login with Google</span>
             </button>
         """
-    st.markdown(button_with_image, on_click=st.login unsafe_allow_html=True)
+    st.markdown(button_with_image, on_click=st.login, unsafe_allow_html=True)
 user = st.experimental_user
 if not user.is_logged_in:
     login_screen()
