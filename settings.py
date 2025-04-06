bd7cc0d9e4dcc3e1e9ecce5dc02f7557d86c4e18
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-04-05 18:42:46 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import streamlit as st
 def login_screen():
     st.header("This app is private.")
     st.subheader("Please log in.")
-    st.button("Log in with Microsoft", on_click=st.login)
+    st.button("Log in with Google", on_click=st.login)
 
 if not st.experimental_user.is_logged_in:
     login_screen()
